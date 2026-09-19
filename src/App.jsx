93f44b23b217823a110bb9b28bedb177870df22e
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-09-19 16:05:59 +0530

    fix: changed heading3

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started with react development. Making change to see what happens in PR.</h1>
+          <h1>Get started with react development. Making change to see what happens in PR. More changes to test.</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
